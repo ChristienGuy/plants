471f1731d43208c3f7dd7bf2767cd60eff6d95d1
Chris Guy <christien.guy@gmail.com>
2018-03-20 16:33:02 +0000

    Add dependencies and changes for PushNotifications on Android

--- android/app/src/main/java/com/plants/MainApplication.java
+++ android/app/src/main/java/com/plants/MainApplication.java
@@ -2,6 +2,7 @@ package com.plants;
 
 import android.app.Application;
 
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+              new ReactNativePushNotificationPackage()
       );
     }
 
