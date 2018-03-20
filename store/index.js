635176d3e01b6904a33fe9a786a1c083a3e1d591
Chris Guy <christien.guy@gmail.com>
2018-03-20 16:33:17 +0000

    Stop persisting state of navigation

--- store/index.js
+++ store/index.js
@@ -8,7 +8,7 @@ import storage from "redux-persist/lib/storage"
 const persistConfig = {
   key: 'root',
   storage,
-  // whitelist: ['plants']
+  whitelist: ['plants']
 }
 
 const appReducer = combineReducers({
