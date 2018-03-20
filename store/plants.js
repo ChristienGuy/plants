c744813da34c8af4d44cefe90b486f4c57f29a89
Chris Guy <christien.guy@gmail.com>
2018-03-20 16:33:58 +0000

    Add typing info for Plant Add initial side-effect to create initial notification for a new plant

--- store/plants.js
+++ store/plants.js
@@ -1,12 +1,19 @@
+// @flow
 import v4 from "uuid/v4";
+import PushNotification from "react-native-push-notification";
 
-initialState = [];
+type Plant = {
+  name: string,
+  lastWatered: Date,
+  daysBetweenWater: number
+};
 
 // Actions
 const ADD = "plants-app/plants/ADD";
 const DELETE = "plants-app/plants/DELETE";
 
 // Reducer
+initialState = [];
 export default (reducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case ADD: {
@@ -31,14 +38,28 @@ export default (reducer = (state = initialState, action = {}) => {
  * Adds a plant to the data store and gives it a uuid
  * @param {object} plant - An object containing all info about a single plant
  */
-export const addPlant = plant => {
+export const addPlant = (plant: Plant) => {
   const id = v4();
+  const newPlant = {
+    id,
+    ...plant
+  };
+
+  // Multiple side-effects in action creator is hard to test.
+  // Look into thunk/saga
+  PushNotification.localNotificationSchedule({
+    message: `${plant.name} needs watering!`,
+    date: new Date(Date.now() + 10 * 1000), // TODO: get date from AddPlantForm
+    userInfo: {
+      plant: {
+        ...newPlant
+      }
+    }
+  });
+
   return {
     type: ADD,
-    plant: {
-      id,
-      ...plant
-    }
+    plant: newPlant
   };
 };
 
