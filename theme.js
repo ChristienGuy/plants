ad2b85fb63601d411a883c4af56edf9c84d379b5
Chris Guy <christien.guy@gmail.com>
2018-06-22 15:10:36 +0100

    Add theme file

---
+++ theme.js
@@ -0,0 +1,20 @@
+const colors = {
+  primaryGreen: "#76CC60",
+  paleGreen: "#d1edca",
+  darkGreen: "#77945D",
+  primaryBlue: "#377989",
+  red: "#EE0C1C",
+  white: "#ffffff",
+  darkGray: "#333333"
+};
+
+const theme = {
+  colors: colors,
+  palette: {
+    textDark: colors.darkGray,
+    textLight: colors.white,
+    danger: colors.red,
+  }
+};
+
+export default theme;
